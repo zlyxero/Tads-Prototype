793ca77330895e9caba0ec8da62388295a3b6646
jeff <jeffymitchy@gmail.com>
2019-02-27 13:23:07 +0300

    set debug to True for troubleshooting deployed app

--- tads_simulation_project/settings.py
+++ tads_simulation_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qehjdz#!954g$eu4@(hemu9#&dlx4^ibpfc8vfrtjuc2mszd10'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
